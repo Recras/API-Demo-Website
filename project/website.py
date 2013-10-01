9202a36c969f1251731bf6c73aa1515003039146
Tijmen Klein <timmipetit@gmail.com>
2013-10-01 13:25:02 +0200

    Added page for 'producten

--- project/website.py
+++ project/website.py
@@ -24,6 +24,14 @@ def arrangement(arrangement_id):
 		arrangement = r['results'][0]	
 		return render_template('arrangement.html', arrangement = arrangement)
 
+@app.route('/producten')
+def producten():
+	url = '%s/api/json/producten/' % app.config['RECRAS_URL']
+	r = requests.get(url, verify=app.config['RECRAS_CRT']).json()
+	if r['succes']:
+		producten = r['results']	
+		return render_template('producten.html', producten = producten)
+
 if __name__ == "__main__":
 	app.debug = True
 	app.run()
