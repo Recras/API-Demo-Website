f149a3547f01600690e065341149b531ed226c1b
Tijmen Klein <timmipetit@gmail.com>
2013-12-17 11:36:24 +0100

    Betere escape

--- project/website.py
+++ project/website.py
@@ -1,12 +1,9 @@
 from flask import Flask, render_template, redirect
-from jinja2 import evalcontextfilter, Markup, escape
+from jinja2 import evalcontextfilter, Markup
 import requests
 import base64
 import data
 import re
-
-_paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
-
 
 app = Flask(__name__)
 app.config['RECRAS_URL'] = 'https://demo.recras.nl/'
@@ -73,11 +70,12 @@ def beschikbaarheidvergaderruimte():
 @app.template_filter()
 @evalcontextfilter
 def nl2br(eval_ctx, value):
-    result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n') \
-        for p in _paragraph_re.split(escape(value)))
-    if eval_ctx.autoescape:
-        result = Markup(result)
-    return result
+	_paragraph_re = re.compile(r'(?:\r\n|\r(?!\n)|\n){2,}')
+	result = u'\n\n'.join(u'<p>%s</p>' % p.replace(u'\n', Markup('<br>\n'))
+										for p in _paragraph_re.split(value))
+	if eval_ctx.autoescape:
+		result = Markup(result)
+	return result
 
 if __name__ == "__main__":
 	app.debug = True
